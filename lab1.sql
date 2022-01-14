39a88a3779a02e6f550b09729162b1b09402ece9
Mauricio Ramirez Esparron <95831941+MauricioEsparron@users.noreply.github.com>
2022-01-14 17:55:50 -0500

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,12 +1,13 @@
 /*Cree una base de datos llamada BD_COMERCIAL teniendo en cuenta las siguientes 
 opciones:
-Un archivo físico y el archivo lógico se creará automáticamente.
-Valide la existencia de la base de datos.
-Asigne al archivo físico el nombre (NAME) “BD_COMERCIAL_PRI” al archivo 
-principal con un tamaño inicial de 50MB (SIZE), un tamaño máximo aceptado por 
-el sistema (MAXSIZE), una tasa de crecimiento del 10% (FILEGROWTH) y debe 
-ser guardado en la carpeta C:\COMERCIAL (FILENAME).
-Verifique la existencia de los archivos implementados.*/
+
+   - Un archivo físico y el archivo lógico se creará automáticamente.
+   - Valide la existencia de la base de datos.
+   - Asigne al archivo físico el nombre (NAME) “BD_COMERCIAL_PRI” al archivo 
+     principal con un tamaño inicial de 50MB (SIZE), un tamaño máximo aceptado por 
+     el sistema (MAXSIZE), una tasa de crecimiento del 10% (FILEGROWTH) y debe 
+     ser guardado en la carpeta C:\COMERCIAL (FILENAME).
+   - Verifique la existencia de los archivos implementados.*/
 
 
 IF DB_ID('BD_COMERCIAL') IS NOT NULL 
