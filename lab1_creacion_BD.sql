f971d06a238278d4e23623fb7dfa409d0474eef4
Mauricio Ramirez Esparron <95831941+MauricioEsparron@users.noreply.github.com>
2022-02-02 23:53:02 -0500

    Update and rename lab1_creacion,modificacion,eliminacion.sql to lab1_creacion_BD.sql

--- lab1_creacion_BD.sql
+++ lab1_creacion_BD.sql
@@ -3,7 +3,7 @@
 /*
 NOTA:
 
- archivo principal		      -> ON	            -> .MDF
+ Archivo principal		        -> ON	            -> .MDF
  Archivo de datos:          -> ON PRIMARY     -> .MDF
  Archivo secunda,rio:       -> ),(            -> .NDF
  Archivo de transacciones:  -> LOG ON         -> .LDF
@@ -183,73 +183,3 @@ GO
 SP_HELPDB BD_TIENDA
 GO
 
-/*------------------------------------------------------------*/
-/*-------------------MODIFICACIÓN-----------------------------*/
-/*------------------------------------------------------------*/
-
---Caso 1: Modificar el nombre de la base de datos BD_TIENDA por MINIMARKET
-USE MASTER
-GO
-ALTER DATABASE BD_TIENDA
-MODIFY NAME=MINIMARKET
-GO
-
-/*---------------------------------------------------------------*/
-
---Caso 2: Agregar 2 archivos secundarios a la base de datos COMERCIAL2017
-ALTER DATABASE COMERCIAL2017
-ADD FILE
-(
-NAME=COMERCIAL_SEC1,
-FILENAME='C:\COMERCIAL\SEC\COMERCIAL_SEC1.NDF',
-SIZE=10,
-MAXSIZE=50,
-FILEGROWTH=10%
-),
-(
-NAME=COMERCIAL_SEC2,
-FILENAME='C:\COMERCIAL\SEC\COMERCIAL_SEC2.NDF',
-SIZE=10,
-MAXSIZE=100,
-FILEGROWTH=15%
-)
-GO
---VERIFICANDO LOS ARCHIVOS CREADOS
-SP_HELPDB COMERCIAL2017
-GO
-
-/*---------------------------------------------------------------*/
-
---Caso 3: Eliminar el archivo secundario COMERCIAL_SEC2 de la base de datos COMERCIAL2017
-ALTER DATABASE COMERCIAL2017
-REMOVE FILE COMERCIAL_SEC2
-GO
---VERIFICANDO LOS ARCHIVOS
-SP_HELPDB COMERCIAL2017
-GO
-
-/*----------------------------------------------------------------*/
-
---SINTAXIS PARA LA MODIFICACIÓN DEL NOMBRE DE UNA ARCHIVO
-ALTER DATABASE [BaseDeDatos]       
-MODIFY FILE (NAME=N’NombreAnterior’, NEWNAME=N’NuevoNombre’)
-GO
-
-/*-----------------------------------------------------------------*/
-                                   
-/*------------------------------------------------------------*/
-/*-------------------ELIMINACIÓN------------------------------*/
-/*------------------------------------------------------------*/
-
---Caso 1: Eliminar la base de datos BD_EJEMPLO
-DROP DATABASE BD_FARMACIA
-GO
-
-/*---------------------------------------------------------------*/
-
---Caso 2: Eliminar la base de datos BD_COMERCIAL validando la existencia del mismo.
-USE MASTER
-GO
-IF DB_ID('BD_COMERCIAL') IS NOT NULL
-DROP DATABASE BD_COMERCIAL
-GO
