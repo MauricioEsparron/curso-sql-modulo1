16595c00e7a14c6046f897f27e02b52fc42d58eb
Mauricio Ramirez Esparron <95831941+MauricioEsparron@users.noreply.github.com>
2022-02-03 00:09:25 -0500

    Update lab4.creación_TB.sql

--- lab4.creación_TB.sql
+++ lab4.creación_TB.sql
@@ -15,68 +15,68 @@ GO
 mejor manera la creación de la tabla, podemos mencionar:
 Asignación de valores nulos y no nulos a las columnas:*/
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
                 CAMPO1 TIPO NULL | NOT NULL,
                 CAMPO2 TIPO NULL | NOT NULL,
                 CAMPO3 TIPO NULL | NOT NULL
 )
-GO*/
+GO
 
 /*----------------------------------------------------------*/
 
 --Asignación de llave primaria:
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
                 CAMPO1 TIPO NOT NULL PRIMARY KEY,
                 CAMPO2 TIPO NULL | NOT NULL,
                 CAMPO3 TIPO NULL | NOT NULL
 )
-GO*/
+GO
 
 /*----------------------------------------------------------*/
 
 --Asignación de llave compuesta:
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
               CAMPO1 TIPO NOT NULL,
               CAMPO2 TIPO NOT NULL,
               CAMPO3 TIPO NULL | NOT NULL,
               PRIMARY KEY (CAMPO1, CAMPO2)
 )
-GO*/
+GO
 
 /*----------------------------------------------------------*/
 
 --Asignación de llave FORÁNEA:
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
               CAMPO1 TIPO NOT NULL PRIMARY KEY,
               CAMPO2 TIPO NULL | NOT NULL,
               CAMPO3 TIPO NULL REFERENCES NOMBRE_TABLA2
 )
-GO*/
+GO
 
 /*----------------------------------------------------------*/
 
 --Asignación de valores por defecto:
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
                 CAMPO1 TIPO NULL | NOT NULL,
                 CAMPO2 TIPO DEFAULT VALOR_POR_DEFECTO,
                 CAMPO3 TIPO NULL | NOT NULL
 )
-GO*/
+GO
 
 /*----------------------------------------------------------*/
 
 --Asignación de restricciones:
 
-/*CREATE TABLE NOMBRE_TABLA (
+CREATE TABLE NOMBRE_TABLA (
                 CAMPO1 TIPO NULL | NOT NULL,
                 CAMPO2 TIPO CHECK (CAMPO=VALOR),
                 CAMPO3 TIPO NULL | NOT NULL
 )
-GO*/
+GO
 
 /*-----------------------------------------------------------*/
 
@@ -91,3 +91,7 @@ CREATE TABLE TABLA (
           CAMPO2 VARCHAR (30) NOT NULL
 )
 GO
+
+
+
+
